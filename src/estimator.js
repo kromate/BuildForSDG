8b3f3dd84d3a205b01e734777d0596b606227a13
kromate <akpananthony33@gmail.com>
2020-04-08 04:43:16 +0100

    BuildforSDG

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,28 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+    data: {
+        region: {
+            name: "Africa";
+            avgAge: 19.7;
+            avgDailyIncomeInUSD: 5;
+            avgDailyIncmePopulation: 0.71;
+        };
+        periodType: "days";
+        timeToElapse: 58;
+        reportedCases: 674;
+        population: 66622705;
+        totalHopitalBeds: 1380614;
+    };
+    var curInfect = data.reportedCases;
+    impact: {
+        currentlyInfected: curInfect * 10;
+        infectionsByRequestedTime: (curInfect * 10) * 512;
+    };
+    severeImpact: {
+        currentlyInfected: curInfect * 50
+        infectionsByRequestedTime: (curInfect * 50) * 512;
+    };
+
+}
+
 
 export default covid19ImpactEstimator;
