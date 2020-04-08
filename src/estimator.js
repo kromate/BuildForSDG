5ed57197c350de9299728beb45f2daff9454a885
kromate <akpananthony33@gmail.com>
2020-04-08 13:52:51 +0100

    bugs

--- src/estimator.js
+++ src/estimator.js
@@ -1,2 +1,27 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data, impact, severeImpact) => (
+  {
+    data: {
+      region: {
+        name: 'Africa',
+        avgAge: 19.7,
+        avgDailyIncomeInUSD: 5,
+        avgDailyIncomePopulation: 0.71
+      },
+      periodType: 'days',
+      timeToElapse: 58,
+      reportedCases: 674,
+      population: 66622705,
+      totalHospitalBeds: 1380614
+    }, // the input data you got
+    impact: {
+      currentlyInfected: data.reportedCases * 10,
+      infectionsByRequestedTime: impact.currentlyInfected * 512
+
+    },
+    severeImpact: {
+      currentlyInfected: data.reportedCases * 50,
+      infectionsByRequestedTime: severeImpact.currentlyInfected * 512
+    }
+  });
+
 export default covid19ImpactEstimator;
