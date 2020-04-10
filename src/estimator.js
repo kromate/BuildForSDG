74e7bf1e7945834f1b31cc8bce5b5f70ec7555d2
kromate <akpananthony33@gmail.com>
2020-04-10 17:17:48 +0100

    --

--- src/estimator.js
+++ src/estimator.js
@@ -1,72 +1,111 @@
 
-const covid19ImpactEstimator = (data) => (
-  {
-    // data: {
-    //   region: {
-    //     name: 'Africa',
-    //     avgAge: 19.7,
-    //     avgDailyIncomeInUSD: 5,
-    //     avgDailyIncomePopulation: 0.71
-    //   },
-    //   periodType: 'days',
-    //   timeToElapse: 58,
-    //   reportedCases: 674,
-    //   population: 66622705,
-    //   totalHospitalBeds: 1380614,
-    impact: {
-      currentlyInfected:
-      data.reportedCases * 10,
-      infectionsByRequestedTime:
-      data.reportedCases * 10
-      * (2 ** Math.trunc((data.timeToElapse / 3))),
-      severeCasesByRequestedTime:
-      (data.reportedCases * 10
-        * (2 ** Math.trunc((data.timeToElapse / 3)))) * 0.15,
-      hospitalBedsByRequestedTime:
-      data.totalHospitalBeds - ((data.reportedCases * 10 * 524288) * 0.15),
-      casesForICUByRequestedTime:
-      (data.reportedCases * 10 * 524288) * 0.05,
-      casesForVentilatorsByRequestedTime:
-      (data.reportedCases * 10 * 524288) * 0.02,
-      dollarsInFlight:
-       (data.reportedCases * 10 * 524288) * 30 * 1.5 * 0.65
-    },
-    severeImpact: {
-      currentlyInfected:
-      data.reportedCases * 50,
-      infectionsByRequestedTime:
-      data.reportedCases * 50
-      * (2 ** Math.trunc((data.timeToElapse / 3))),
-      severeCasesByRequestedTime:
-      (data.reportedCases * 50
-        * (2 ** Math.trunc((data.timeToElapse / 3)))) * 0.15,
-      hospitalBedsByRequestedTime:
-       data.totalHospitalBeds
-       - (data.reportedCases * 50 * (2 ** Math.trunc((data.timeToElapse / 3)))) * 0.15,
-      casesForICUByRequestedTime:
-      (data.reportedCases * 50 * 524288) * 0.05,
-      casesForVentilatorsByRequestedTime:
-       (data.reportedCases * 50 * 524288) * 0.02,
-      dollarsInFlight:
-      (data.reportedCases * 50 * 524288) * 30 * 1.5 * 0.65
-    }
-  });
+  
+const covid19ImpactEstimator = (data) => {
+  const {
+    reportedCases,
+    timeToElapse,
+    periodType,
+    totalHospitalBeds,
+    region
+  } = data;
+  const result = {
+    data,
+    impact: {},
+    severeImpact: {}
+  };
 
-// const covid = covid19ImpactEstimator({
+
+  // calculate the number of currently infected for both impact and severe impact
+  const currentlyInfected = (factor) => reportedCases * factor;
+  const infectionsByRequestedTime = (time) => {
+    let t = time;
+    switch (periodType) {
+      case 'days':
+        t *= 1;
+        break;
+      case 'weeks':
+        t *= 7;
+        break;
+      case 'months':
+        t *= 30;
+        break;
+      default:
+        break;
+    }
+    const dollarMultiplierTIme = t;
+    const multiplier = 2 ** Math.floor(t / 3);
+    return { multiplier, dollarMultiplierTIme };
+  };
+  result.impact.currentlyInfected = Math.floor(currentlyInfected(10));
+  result.severeImpact.currentlyInfected = Math.floor(currentlyInfected(50));
+  result.impact.infectionsByRequestedTime = (
+    result.impact.currentlyInfected * infectionsByRequestedTime(timeToElapse).multiplier
+  );
+  result.severeImpact.infectionsByRequestedTime = (
+    result.severeImpact.currentlyInfected * infectionsByRequestedTime(timeToElapse).multiplier
+  );
+  result.impact.severeCasesByRequestedTime = result.impact.infectionsByRequestedTime * 0.15;
+  result.severeImpact.severeCasesByRequestedTime = (
+    result.severeImpact.infectionsByRequestedTime * 0.15
+  );
+  result.impact.hospitalBedsByRequestedTime = (
+    Math.ceil(0.35 * totalHospitalBeds) - result.impact.severeCasesByRequestedTime
+  );
+  result.severeImpact.hospitalBedsByRequestedTime = (
+    Math.ceil(0.35 * totalHospitalBeds) - result.severeImpact.severeCasesByRequestedTime
+  );
+  result.impact.casesForICUByRequestedTime = (
+    result.impact.infectionsByRequestedTime * 0.05
+  );
+  result.severeImpact.casesForICUByRequestedTime = (
+    result.severeImpact.infectionsByRequestedTime * 0.05
+  );
+  result.impact.casesForVentilatorsByRequestedTime = (
+    Math.floor(result.impact.infectionsByRequestedTime * 0.02)
+  );
+  result.severeImpact.casesForVentilatorsByRequestedTime = (
+    Math.floor(result.severeImpact.infectionsByRequestedTime * 0.02)
+  );
+  result.impact.dollarsInFlight = (
+    (
+      result.impact.infectionsByRequestedTime * region.avgDailyIncomePopulation)
+        * region.avgDailyIncomeInUSD * infectionsByRequestedTime(timeToElapse).dollarMultiplierTIme
+  );
+  result.severeImpact.dollarsInFlight = (
+    (
+      result.severeImpact.infectionsByRequestedTime * region.avgDailyIncomePopulation)
+        * region.avgDailyIncomeInUSD * infectionsByRequestedTime(timeToElapse).dollarMultiplierTIme
+  );
+  console.log(result);
+  return result;
+};
+
+// covid19ImpactEstimator({
 //   region: {
 //     name: 'Africa',
 //     avgAge: 19.7,
-//     avgDailyIncomeInUSD: 4,
-//     avgDailyIncomePopulation: 0.73
+//     avgDailyIncomeInUSD: 2,
+//     avgDailyIncomePopulation: 0.78
 //   },
-//   periodType: 'days',
-//   timeToElapse: 38,
-//   reportedCases: 2747,
-//   population: 92931687,
-//   totalHospitalBeds: 678874
+//   reportedCases: 3108,
+//   population: 117443211,
+//   totalHospitalBeds: 912638,
+//   timeToElapse: 98,
+//   periodType: 'days'
+// });
+// covid19ImpactEstimator({
+//   region: {
+//     name: 'Africa',
+//     avgAge: 19.7,
+//     avgDailyIncomeInUSD: 3,
+//     avgDailyIncomePopulation: 0.66
+//   },
+//   reportedCases: 1784,
+//   population: 148869565,
+//   totalHospitalBeds: 1993352,
+//   timeToElapse: 2,
+//   periodType: 'months'
 // });
 
-// console.log(covid.impact.infectionsByRequestedTime);
+module.exports = covid19ImpactEstimator;
 
-
-export default covid19ImpactEstimator;
---
+++ src/test.js
@@ -0,0 +1,80 @@
+
+const covid19ImpactEstimator = (data) => (
+  {
+    // data: {
+    //   region: {
+    //     name: 'Africa',
+    //     avgAge: 19.7,
+    //     avgDailyIncomeInUSD: 5,
+    //     avgDailyIncomePopulation: 0.71
+    //   },
+    //   periodType: 'days',
+    //   timeToElapse: 58,
+    //   reportedCases: 674,
+    //   population: 66622705,
+    //   totalHospitalBeds: 1380614,
+     
+
+    impact: {
+      currentlyInfected:
+      data.reportedCases * 10,
+      infectionsByRequestedTime: 
+      data.reportedCases * 10
+      * (2 ** Math.trunc((data.timeToElapse / 3))),
+      severeCasesByRequestedTime:
+      (data.reportedCases * 10
+        * (2 ** Math.trunc((data.timeToElapse / 3)))) * 0.15,
+      hospitalBedsByRequestedTime:
+      data.totalHospitalBeds - ((data.reportedCases * 10 * 524288) * 0.15),
+      casesForICUByRequestedTime:
+      (data.reportedCases * 10 * 524288) * 0.05,
+      casesForVentilatorsByRequestedTime:
+      (data.reportedCases * 10 * 524288) * 0.02,
+      dollarsInFlight:
+       (data.reportedCases * 10 * 524288) * 30 * 1.5 * 0.65,
+       
+    },
+    severeImpact: {
+      currentlyInfected:
+      data.reportedCases * 50,
+      infectionsByRequestedTime:
+      data.reportedCases * 50
+      * (2 ** Math.trunc((data.timeToElapse / 3))),
+      severeCasesByRequestedTime:
+      (data.reportedCases * 50
+        * (2 ** Math.trunc((data.timeToElapse / 3)))) * 0.15,
+      hospitalBedsByRequestedTime:
+       data.totalHospitalBeds
+       - (data.reportedCases * 50 * (2 ** Math.trunc((data.timeToElapse / 3)))) * 0.15,
+      casesForICUByRequestedTime:
+      (data.reportedCases * 50 * 524288) * 0.05,
+      casesForVentilatorsByRequestedTime:
+       (data.reportedCases * 50 * 524288) * 0.02,
+      dollarsInFlight:
+      (data.reportedCases * 50 * 524288) * 30 * 1.5 * 0.65
+    }
+  },
+  function per((data.periodType)) { 
+    
+  });
+
+
+
+// const covid = covid19ImpactEstimator({
+//   region: {
+//     name: 'Africa',
+//     avgAge: 19.7,
+//     avgDailyIncomeInUSD: 4,
+//     avgDailyIncomePopulation: 0.73
+//   },
+//   periodType: 'days',
+//   timeToElapse: 38,
+//   reportedCases: 2747,
+//   population: 92931687,
+//   totalHospitalBeds: 678874
+// });
+
+// console.log(covid.impact.infectionsByRequestedTime);
+
+
+export default covid19ImpactEstimator;
